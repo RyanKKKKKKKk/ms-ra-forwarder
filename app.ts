9b9fa97bdc799134cc4a8a1ce5b34861c9679e6e
RyanKKKKKKKk <125950356+RyanKKKKKKKk@users.noreply.github.com>
2023-05-05 17:11:36 +0800

    Update app.ts

--- app.ts
+++ app.ts
@@ -4,6 +4,14 @@ import * as cors from 'cors';
 
 const app = express()
 app.use(cors())
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});  
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000
 
 app.use(bodyParser.text({ type: '*/*' }))
